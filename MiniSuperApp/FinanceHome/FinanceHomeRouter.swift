521b6d19694df0c58cbe39769b777b1a0c11a4a9
eunjin <eunjin3786@naver.com>
2021-11-01 18:01:56 +0900

    superpaydashboard ui 작업

--- MiniSuperApp/FinanceHome/FinanceHomeRouter.swift
+++ MiniSuperApp/FinanceHome/FinanceHomeRouter.swift
@@ -1,19 +1,38 @@
 import ModernRIBs
 
-protocol FinanceHomeInteractable: Interactable {
-  var router: FinanceHomeRouting? { get set }
-  var listener: FinanceHomeListener? { get set }
+protocol FinanceHomeInteractable: Interactable, SuperPayDashboardListener {
+    var router: FinanceHomeRouting? { get set }
+    var listener: FinanceHomeListener? { get set }
 }
 
 protocol FinanceHomeViewControllable: ViewControllable {
-  // TODO: Declare methods the router invokes to manipulate the view hierarchy.
+    func addDashboard(_ view: ViewControllable)
 }
 
 final class FinanceHomeRouter: ViewableRouter<FinanceHomeInteractable, FinanceHomeViewControllable>, FinanceHomeRouting {
-  
-  // TODO: Constructor inject child builder protocols to allow building children.
-  override init(interactor: FinanceHomeInteractable, viewController: FinanceHomeViewControllable) {
-    super.init(interactor: interactor, viewController: viewController)
-    interactor.router = self
-  }
+    
+    private let superPayDashboardBuilder: SuperPayDashboardBuildable
+    private var superPayRouting: Routing?
+    
+    init(interactor: FinanceHomeInteractable,
+                  viewController: FinanceHomeViewControllable,
+                  superPayDashboardBuilder: SuperPayDashboardBuildable) {
+        self.superPayDashboardBuilder = superPayDashboardBuilder
+        super.init(interactor: interactor, viewController: viewController)
+        interactor.router = self
+    }
+    
+    func attachSuperPayDashboard() {
+        if superPayRouting != nil {
+            return
+        }
+        
+        let router = superPayDashboardBuilder.build(withListener: interactor)
+        
+        let dashboard = router.viewControllable
+        viewController.addDashboard(dashboard)
+        
+        superPayRouting = router
+        attachChild(router)
+    }
 }
